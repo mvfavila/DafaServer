1e4a3e87a1af92605b4aec72c85d19b3ad174221
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2018-12-22 19:20:52 +0100

    Added EventType model

---
+++ models/EventType.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var EventTypeSchema = new mongoose.Schema({
+  id: { type: mongoose.Schema.Types.ObjectId },
+  name: { type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true },
+  numberOfDaysToWarning: { type: Number, required: [true, "can't be blank"], match: [/^[0-9]+$/, 'is invalid'] },
+  createdAt: { type: mongoose.Schema.Types.Date },
+  updatedAt: { type: mongoose.Schema.Types.Date },
+  active: { type: mongoose.Schema.Types.Boolean },
+}, {timestamps: true});
+
+EventTypeSchema.plugin(uniqueValidator, {message: 'is already taken.'});
+
+EventTypeSchema.methods.toAuthJSON = function(){
+  return {
+    id: this.id,
+    name: this.name,
+    numberOfDaysToWarning: this.numberOfDaysToWarning,
+    createdAt: this.createdAt,
+    updatedAt: this.updatedAt,
+    active: this.active,
+  };
+};
+
+mongoose.model('EventType', EventTypeSchema);
