e22b70013841ac55d83d4e9821a66ff59d08c73d
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2018-12-23 12:59:22 +0100

    GetClientById fetches the active Fields of the Client

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,9 +1,19 @@
 var mongoose = require('mongoose');
 var Client = mongoose.model('Client');
+var Field = mongoose.model('Field');
 
 var clientController = {
     getClientById(clientId) {
-        return Client.findById(clientId);
+        return Client.findById(clientId).then(function(client){
+            if(client == null) return new Promise();
+
+            return new Promise((resolve, reject) => {
+                Field.find({ clientId: clientId, active: true }).then(function(fields){
+                    client.fields = fields;
+                    resolve(client);
+                });
+            });
+        });
     },
 
     getAllClients() {
