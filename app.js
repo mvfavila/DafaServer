0007a8a36b32c3c9ba8f3a70a15af2a6c298da69
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2018-12-25 11:18:42 +0100

    Added body-parser

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ var express  = require('express');                     // create our app w/ expr
 var mongoose = require('mongoose');                    // mongoose for mongodb
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var logger = require('morgan');                        // log requests to the console (express4)
+var bodyParser = require('body-parser');               // pull information from HTML POST (express4)
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -21,10 +25,10 @@ app.use('/users', usersRouter);
 var isProduction = process.env.NODE_ENV === 'production';
 
 if(isProduction){
-    mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 } else {
     // Configuration
-    mongoose.connect('mongodb://firstUser:Abc123!@ds121652.mlab.com:21652/dafadb'); 
+    mongoose.connect('mongodb://firstUser:Abc123!@ds121652.mlab.com:21652/dafadb', { useNewUrlParser: true }); 
     mongoose.set('debug', true);
 }
 
