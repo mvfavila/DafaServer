4dc2c854a97a447cf461e2b7b7b2192645bc0bb1
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-01-12 09:06:12 +0100

    Rolled back util changes

--- util/util.js
+++ util/util.js
@@ -1,14 +1,16 @@
-export const httpStatus = {
-    SUCCESS: 200,
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    NOT_FOUND: 404,
-    UNPROCESSABLE_ENTITY: 422,
-    INTERNAL_SERVER_ERROR: 500,
+module.exports = {
+    httpStatus: {
+        SUCCESS: 200,
+        BAD_REQUEST: 400,
+        UNAUTHORIZED : 401,
+        NOT_FOUND: 404,
+        UNPROCESSABLE_ENTITY: 422,
+        INTERNAL_SERVER_ERROR: 500,
+    },
+    presentableErrorCodes: [
+        400,
+        401,
+        404,
+        422,
+    ], 
 };
-export const presentableErrorCodes = [
-    400,
-    401,
-    404,
-    422,
-];
