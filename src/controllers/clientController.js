ee63b2735c0de51dbef3fc83440a51b4f787b0c3
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-01-23 19:47:06 +0100

    UpdateClient test added

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -3,40 +3,45 @@ var Client = mongoose.model('Client');
 var Field = mongoose.model('Field');
 
 var clientController = {
-    getClientById(clientId) {
-        return Client.findById(clientId).then(function(client){
-            if(client == null) return new Promise((resolve, reject) => { resolve(null) });
-
+    async getClientById(clientId) {
+        try {
+            const client = await Client.findById(clientId);
+            if (client == null)
+                return new Promise((resolve, reject) => { resolve(null); });
             return new Promise((resolve, reject) => {
-                Field.find({ clientId: clientId, active: true }).then(function(fields){
+                Field.find({ clientId: clientId, active: true }).then(function (fields) {
                     client.fields = fields;
                     resolve(client);
-                });
+                }).catch((err) => { throw err; });
+                ;
             });
-        });
+        }
+        catch (err_1) {
+            throw err_1;
+        };
     },
 
     getAllClients() {
         return Client.find();
     },
 
-    addClient(client){
+    addClient(client) {
         client.active = true;
         return client.save();
     },
 
-    updateClientStatus(client){
-        this.getClientById(client.clientId).then(function(foundClient) {
-            if(client == null) return new Promise((resolve, reject) => { resolve(null) });
+    async updateClientStatus(client) {
+        await this.getClientById(client._id).then(function (foundClient) {
+            if (client == null) return new Promise((resolve, reject) => { resolve(null) });
 
             foundClient.active = client.active;
             return foundClient.save();
-        })
+        }).catch((err) => { throw err; });
     },
 
-    updateClient(client){
-        this.getClientById(client.clientId).then(function(foundClient) {
-            if(client == null) return new Promise((resolve, reject) => { resolve(null) });
+    async updateClient(client) {
+        await this.getClientById(client._id).then((foundClient) => {
+            if (foundClient == null) return new Promise((resolve, reject) => { resolve(null); });
 
             foundClient.firstName = client.firstName;
             foundClient.lastName = client.lastName;
@@ -47,9 +52,8 @@ var clientController = {
             foundClient.postalCode = client.postalCode;
             foundClient.email = client.email;
             foundClient.active = client.active;
-
             return foundClient.save();
-        })
+        }).catch((err) => { throw err; });
     },
 }
 
--- src/controllers/clientController.spec.js
+++ src/controllers/clientController.spec.js
@@ -34,8 +34,8 @@ after(() => {
 });
 
 describe('Client controller', () => {
-  
-  it("clientController - Add client - Should succeed", async () => {
+
+  it("clientController - Add client - Must succeed", async () => {
     let cnt = await Client.countDocuments();
 
     // Dataset must be empty
@@ -52,12 +52,65 @@ describe('Client controller', () => {
     client.postalCode = '12000-000';
     client.email = 'email@domain.com';
 
-    await clientController.addClient(client).then(async function(){
-        await Client.countDocuments().then(function(count){
-          // Dataset must have exactly one item
-          expect(count).to.equal(1);
-        }).catch(_ => { throw new Error(); });      
-    }).catch(_ => { throw new Error(); });
+    await clientController.addClient(client).then(async function () {
+      await Client.countDocuments().then(function (count) {
+        // Dataset must have exactly one item
+        expect(count).to.equal(1);
+      }).catch((err) => { throw new Error(err); });
+    }).catch((err) => { throw new Error(err); });
   });
-  
+
+  it("clientController - Get all clients - Must return 1 client", async () => {
+    let cnt = await Client.countDocuments();
+
+    // Dataset must be empty
+    expect(cnt).to.equal(1);
+
+    await clientController.getAllClients().then(function (clients) {
+      // Must return exactly one client
+      expect(clients.length).to.equal(1);
+    }).catch((err) => { throw new Error(err); });
+  });
+
+  it("clientController - Update client - Must succeed", async () => {
+    let cnt = await Client.countDocuments();
+
+    // Dataset must be empty
+    expect(cnt).to.equal(1);
+
+    clientController.getAllClients().then(async function(clients){
+
+        const client = clients[0];
+        client.firstName = 'New First Name';
+        client.lastName = 'New Last Name';
+        client.company = 'New Company SA';
+        client.address = 'Street 2';
+        client.city = 'London';
+        client.state = 'Alagoas';
+        client.postalCode = '12000-001';
+        client.email = 'new@domain.com';
+        client.active = false;
+
+        await clientController.updateClient(client);
+
+        clientController.getAllClients().then(function(newFoundClients){
+          
+          const updClient = newFoundClients[0];
+          expect(updClient._id.toString()).to.equal(client._id.toString());
+          expect(updClient.firstName).to.equal(client.firstName);
+          expect(updClient.lastName).to.equal(client.lastName);
+          expect(updClient.company).to.equal(client.company);
+          expect(updClient.address).to.equal(client.address);
+          expect(updClient.city).to.equal(client.city);
+          expect(updClient.state).to.equal(client.state);
+          expect(updClient.postalCode).to.equal(client.postalCode);
+          expect(updClient.email).to.equal(client.email);
+          expect(updClient.active).to.equal(client.active);
+    
+        }).catch((err) => { throw err; });
+
+    }).catch((err) => { throw err; });
+    
+  });
+
 });
