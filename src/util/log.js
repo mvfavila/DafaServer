3b67ed1c51cf2b78734a4def77847168ff23fe75
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-05-21 23:13:43 +0200

    Added log-to-file npm package and fixed npm packages vulnerabilities

---
+++ src/util/log.js
@@ -0,0 +1,62 @@
+const log = require("log-to-file");
+
+/**
+ * Auxiliary function to get current date.
+ */
+function getTodayDate() {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0"); // January is 0!
+  const yyyy = today.getFullYear();
+
+  return yyyy + mm + dd;
+}
+
+/**
+ * Auxiliary method to write log message to file.
+ * @param {string} message Message to be logged.
+ * @param {string} level Log level.
+ */
+function writeLog(message, level) {
+  const fileName = `anagramApp_${getTodayDate()}.log`;
+  log(`${level}: ${message}`, fileName);
+}
+
+/**
+ * AnagramApp Logger
+ */
+const logger = {
+  /**
+   * Info level log function.
+   * @param {string} message Message to be logged.
+   */
+  info(message) {
+    const level = "INFO";
+    writeLog(message, level);
+  },
+
+  /**
+   * Debug level log function.
+   * @param {string} message Message to be logged.
+   */
+  debug(message) {
+    const isDebugModeOn =
+      (process.env.DEBUG || "false").toLowerCase() === "true";
+    const level = "DEBUG";
+
+    if (isDebugModeOn) {
+      writeLog(message, level);
+    }
+  },
+
+  /**
+   * Error level log function.
+   * @param {string} message Message to be logged.
+   */
+  error(message) {
+    const level = "ERROR";
+    writeLog(message, level);
+  }
+};
+
+module.exports = logger;
