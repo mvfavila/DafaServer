0f5ed9827c075e739fdf87e98777e0ab02ac0558
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-05-26 20:00:44 +0200

    Changed eventWarning api to ES8

--- src/routes/api/eventWarnings.js
+++ src/routes/api/eventWarnings.js
@@ -6,51 +6,48 @@ const auth = require("../auth");
 const eventWarningController = require("../../controllers/eventWarningController");
 const { httpStatus } = require("../../util/util");
 
-/*
-    GET
-    Health check for the Event Warning endpoint
-*/
+/**
+ * (GET) Health check for the Event Warning endpoint.
+ */
 function getHealthCheck(req, res) {
   return res.sendStatus(httpStatus.SUCCESS);
 }
 
-/*
-    GET
-    Get all active event warnings
-*/
-function getEventWarningsFields(req, res, next) {
-  eventWarningController
-    .getEventWarningsFields()
-    .then(eventWarnings => {
-      if (!eventWarnings) {
-        return res
-          .status(httpStatus.UNAUTHORIZED)
-          .send({ error: "No event warning found" });
-      }
+/**
+ * (GET) Get all active event warnings.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ */
+async function getEventWarningsFields(req, res) {
+  const eventWarnings = await eventWarningController.getEventWarningsFields();
 
-      const eventWarningsJson = [];
-      eventWarnings.forEach(eventWarning => {
-        eventWarningsJson.push(eventWarning);
-      });
+  if (!eventWarnings) {
+    return res
+      .status(httpStatus.UNAUTHORIZED)
+      .send({ error: "No event warning found" });
+  }
 
-      return res.json({ eventWarnings: eventWarningsJson });
-    })
-    .catch(next);
+  const eventWarningsJson = [];
+  eventWarnings.forEach(eventWarning => {
+    eventWarningsJson.push(eventWarning);
+  });
+
+  return res.json({ eventWarnings: eventWarningsJson });
 }
 
-/*
-    POST
-    Creates a new Event Warning
-*/
-function createEventWarning(req, res, next) {
+/**
+ * (POST) Creates a new Event Warning.
+ * @param {Object} req Request object.
+ * @param {Object} res Response object.
+ */
+async function createEventWarning(req, res) {
   const eventWarning = new EventWarning();
 
   eventWarning.event = req.body.eventWarning.eventId;
 
-  eventWarningController
-    .addEventWarning(eventWarning)
-    .then(() => res.json({ eventWarning: eventWarning.toAuthJSON() }))
-    .catch(next);
+  await eventWarningController.addEventWarning(eventWarning);
+
+  res.json({ eventWarning: eventWarning.toAuthJSON() });
 }
 
 // Routers
