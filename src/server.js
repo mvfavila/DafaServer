6d06747d79d7e4f075d7be85f0fa7650e5d7b843
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-07-28 19:51:53 +0200

    CORS config - added middleware to add headers

--- src/server.js
+++ src/server.js
@@ -113,6 +113,26 @@ if (!isTest) {
 //   next();
 // });
 
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,authorization"
+  );
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // require models
 require("./models/User");
 require("./models/Client");
