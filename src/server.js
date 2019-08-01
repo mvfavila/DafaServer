066dce8a5055f32f578ba889b686964d51e63854
Marcos Vinicius Fontes de Avila <mvfavila@gmail.com>
2019-08-01 22:22:48 +0200

    CORS config

--- src/server.js
+++ src/server.js
@@ -97,7 +97,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Request-With, Content-Type, Accept, Authorization"
+    "Origin, X-Request-With, Content-Type, Accept, Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token, X-Amz-User-Agent, Access-Control-Allow-Credentials"
   );
   if (req.method === "OPTIONS") {
     res.header(
